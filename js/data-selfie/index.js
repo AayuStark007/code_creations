9ae266d6f715694a28e8401bebfadfe0f4734ab5
Aayush Gupta <aayu.gupta.96@gmail.com>
2019-06-03 23:49:45 +0530

    post geolocation to server

--- js/data-selfie/index.js
+++ js/data-selfie/index.js
@@ -4,3 +4,21 @@ const app = express();
 
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+
+/* 
+Need: 
+- Routing
+- JSON Parsing
+- POST with fetch()
+*/
+
+app.post("/api", (request, response) => {
+  console.log(request.body);
+  const data = request.body;
+  response.json({
+    status: "success",
+    latitude: data.latitude,
+    longitude: data.longitude
+  });
+});
