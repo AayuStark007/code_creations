f105ee5c794fb475dd7474ba51391454738335a1
Aayush Gupta <aayu.gupta.96@gmail.com>
2019-06-04 20:59:05 +0530

    add database

--- js/data-selfie/index.js
+++ js/data-selfie/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 const express = require("express");
-const app = express();
+const Datastore = require("nedb");
 
+const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
@@ -13,11 +14,18 @@ Need:
 - POST with fetch()
 */
 
+const database = new Datastore("database.db");
+database.loadDatabase();
+
 app.post("/api", (request, response) => {
   console.log(request.body);
   const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
   response.json({
     status: "success",
+    timestamp: timestamp,
     latitude: data.latitude,
     longitude: data.longitude
   });
